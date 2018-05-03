bb8863cb9db2dd983067d44bbb2237642e5a697e
SHANAVASH Mohamed <shanavash.ismail@brillio.com>
2018-05-03 16:19:12 +0530

    updating java file

--- JavaAutoBuild.java
+++ JavaAutoBuild.java
@@ -1,6 +1,6 @@
 public class JavaAutoBuild	{
 	public static void main (String[] args) {
-		system.out.porintln("Hello Docker !");
+		system.out.println("Hello Docker !");
 		system.out.println("is Atuomatic Create a Docker Build");
 		
 	}
